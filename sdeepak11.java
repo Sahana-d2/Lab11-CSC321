72048237fc615f5c6828091f222b36ee96ec39f1
Sahana <sdeepak1@toromail.csudh.edu>
2023-11-10 03:04:46 +0000

    Adding a Java file

---
+++ sdeepak11.java
@@ -0,0 +1,20 @@
+// Sahana Deepak
+// 212244814
+// CSC321-Lab9
+
+import java.util.Scanner;
+
+public class sdeepak11 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int psquare = number * number;
+
+        System.out.println("The square of " + number + " is " + psquare);
+        System.out.println("All work and no play makes Jack a dull boy");
+    }
+}
+
