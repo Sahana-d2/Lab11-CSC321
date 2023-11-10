1dd7204e9b0b08f05f0dfb57c8efd30a33a3f000
Sahana <sdeepak1@toromail.csudh.edu>
2023-11-10 03:02:35 +0000

    Adding a python program

---
+++ sdeepak11a.py
@@ -0,0 +1,14 @@
+# Sahana Deepak
+# 212244814
+# Lab#11-CSC321
+
+def psquare(number):
+    return (number * number)
+
+number = int(input("Enter a number: "))
+
+output = psquare(number)
+
+print(f"The square of {number} is {output}")
+print("All work and no play makes Jack a dull boy")
+
